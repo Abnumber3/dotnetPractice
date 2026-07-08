f12292926389b2a23312c1853a90f6402eda2f68
Abnumber3 <114044297+Abnumber3@users.noreply.github.com>
2026-07-07 23:56:19 -0400

    project practice

---
+++ api/Controllers/ProductsController.cs
@@ -0,0 +1,50 @@
+
+using Core.Entities;
+using Core.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using SQLitePCL;
+
+namespace api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        readonly IProductRepository _repo;
+        public ProductsController(IProductRepository repo)
+        {
+            _repo = repo;
+        }
+
+
+        [HttpGet]
+        public async Task<ActionResult<IReadOnlyList<Product>>> GetProducts()
+        {
+            var products = await _repo.GetProductsAsync();
+            return Ok(products);
+        }
+
+
+
+        [HttpGet("{id}")]
+
+        public async Task<ActionResult<Product>> GetProduct(int id)
+        {
+            var product = await _repo.GetProductByIdAsync(id);
+
+            if(product == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(product);
+        }
+
+
+
+
+
+
+    }
+
+}
