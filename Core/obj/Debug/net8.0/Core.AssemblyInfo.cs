53551e89b1679eb96a7601b3f11666ae0dc693c8
Abnumber3 <114044297+Abnumber3@users.noreply.github.com>
2026-07-14 20:28:14 -0400

    specification practice

--- Core/obj/Debug/net8.0/Core.AssemblyInfo.cs
+++ Core/obj/Debug/net8.0/Core.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a349ee1aa57bd75be21b3ec305674018555e7687")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b4483c5025340471ffcf742eca4e8161df693184")]
 [assembly: System.Reflection.AssemblyProductAttribute("Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
