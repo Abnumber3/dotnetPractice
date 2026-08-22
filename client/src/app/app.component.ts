ae1221fa230254630795020c8ef18a8ca116263b
Abnumber3 <114044297+Abnumber3@users.noreply.github.com>
2026-08-22 17:52:42 -0400

    more updates to client http requests interpolation

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'client';
+  title = 'Skinet';
+
+  products: any[] = [];
+
+
+  constructor(private http: HttpClient) {}
+
+
+  ngOnInit(): void {
+
+    this.http.get('https://localhost:5001/api/products').subscribe({
+      next: ((response : any)=>{
+        this.products = response.data;
+        
+        console.log(this.products);
+      }),
+
+      error: ((error)=>{
+        console.log(error);
+      }),
+
+      complete: (()=>{
+        console.log('Request completed');
+      })
+  })
 }
+
+  }
