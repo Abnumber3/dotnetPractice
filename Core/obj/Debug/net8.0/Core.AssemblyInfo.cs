79236629d66b633bfb3fa9bbd419f843cbaf2120
Abnumber3 <114044297+Abnumber3@users.noreply.github.com>
2026-09-13 22:16:17 -0400

    client side template product details section

--- Core/obj/Debug/net8.0/Core.AssemblyInfo.cs
+++ Core/obj/Debug/net8.0/Core.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ac6748522ca39f7d37055a9e1b6e74d7dbe07eae")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+697f7605978c31ac437a9a345d24e59296c76956")]
 [assembly: System.Reflection.AssemblyProductAttribute("Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
