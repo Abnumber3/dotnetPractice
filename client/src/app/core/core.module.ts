715549c131f6a0a06d5adb74dc43685625695e22
Abnumber3 <114044297+Abnumber3@users.noreply.github.com>
2026-09-25 23:05:25 -0400

    added toastr tweaks on client

--- client/src/app/core/core.module.ts
+++ client/src/app/core/core.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from "src/app/app-routing.module";
 import { TestErrorComponent } from './test-error/test-error.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { ServerErrorComponent } from './server-error/server-error.component';
+import { ToastrModule } from 'ngx-toastr';
 
 
 
@@ -17,7 +18,11 @@ import { ServerErrorComponent } from './server-error/server-error.component';
   ],
   imports: [
     CommonModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ToastrModule.forRoot({
+      positionClass: 'toast-bottom-right',
+      preventDuplicates: true
+    })
 ],
 
   exports: [
